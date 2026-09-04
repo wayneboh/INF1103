b8ff4d26bf3be765cb96bf4306208fdd8835828d
Wayne Boh <waynebohsub@gmail.com>
2026-09-04 10:28:47 +0800

    Second Post

--- hello.py
+++ hello.py
@@ -2,3 +2,21 @@ print("====================")
 print("Welcome here")
 print("My first post")
 print("====================")
+
+userName = "User Name Test"
+bio = "Test Bio"
+followers = 100
+
+print("Username: ", userName)
+print("Bio: ", bio)
+print("Followers: ", followers)
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 50
+print("Day 2:", followers)
+
+followers += 50
+print("Day 3:", followers)
+
